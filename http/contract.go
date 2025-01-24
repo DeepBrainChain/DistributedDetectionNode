390c73abeb83505b4096d61388cdc8a0d57b85c4
Jerry <1364812552@qq.com>
2025-01-24 16:19:31 +0800

    feat: Add unimplement online/offline interface

--- http/contract.go
+++ http/contract.go
@@ -68,9 +68,59 @@ func UnregisterMachine(ctx *gin.Context, chain *dbc.DbcChain) {
 }
 
 func OnlineMachine(ctx *gin.Context, chain *dbc.DbcChain) {
-	//
+	rsp := types.BaseHttpResponse{
+		Code:    0,
+		Message: "ok",
+	}
+
+	var req types.ContractReportInfo
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		rsp.Code = int(types.ErrCodeParse)
+		rsp.Message = types.ErrCodeParse.String()
+		ctx.JSON(ohttp.StatusBadRequest, rsp)
+		return
+	}
+	if err := req.Validate(); err != nil {
+		rsp.Code = int(types.ErrCodeParam)
+		rsp.Message = err.Error()
+		ctx.JSON(ohttp.StatusBadRequest, rsp)
+		return
+	}
+
+	if err := chain.Report(types.MachineOnline, req.StakingType, req.ProjectName, req.MachineId); err != nil {
+		rsp.Code = int(types.ErrCodeDbcChain)
+		rsp.Message = err.Error()
+		ctx.JSON(ohttp.StatusInternalServerError, rsp)
+		return
+	}
+	ctx.JSON(ohttp.StatusOK, rsp)
 }
 
 func OfflineMachine(ctx *gin.Context, chain *dbc.DbcChain) {
-	//
+	rsp := types.BaseHttpResponse{
+		Code:    0,
+		Message: "ok",
+	}
+
+	var req types.ContractReportInfo
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		rsp.Code = int(types.ErrCodeParse)
+		rsp.Message = types.ErrCodeParse.String()
+		ctx.JSON(ohttp.StatusBadRequest, rsp)
+		return
+	}
+	if err := req.Validate(); err != nil {
+		rsp.Code = int(types.ErrCodeParam)
+		rsp.Message = err.Error()
+		ctx.JSON(ohttp.StatusBadRequest, rsp)
+		return
+	}
+
+	if err := chain.Report(types.MachineOffline, req.StakingType, req.ProjectName, req.MachineId); err != nil {
+		rsp.Code = int(types.ErrCodeDbcChain)
+		rsp.Message = err.Error()
+		ctx.JSON(ohttp.StatusInternalServerError, rsp)
+		return
+	}
+	ctx.JSON(ohttp.StatusOK, rsp)
 }
